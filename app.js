dc6aae64a566a4bf8775f15fdbf1fbd70f65aa4b
KevinC76 <xckevinxc67@gmail.com>
2025-03-18 15:39:59 +0700

    create mongoDB connection and users model

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 require('express-async-errors');
+require('dotenv').config();
+const mongoose = require('mongoose');
 
 const express = require('express');
 const errorHandler = require('./handlers/errorHandlers');
 
 const app = express();
 
-app.use(express.json());
+mongoose
+  .connect(process.env.mongo_connection, {})
+  .then(() => {
+    console.log('success connect to DB');
+  })
+  .catch(() => {
+    console.log('faild to connect to DB');
+  });
 
-//routes
+//models initialization
+require('./models/users.model');
+
+app.use(express.json());
 
 //error handler
 app.use(errorHandler);
